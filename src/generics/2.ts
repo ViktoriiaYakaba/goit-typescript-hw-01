4dcc869c2ce176dab4b5beef987d54acc59c8a50
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-08 16:12:06 +0200

    update 2.ts, 3.ts

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -5,11 +5,10 @@ type AllType = {
   weight: number;
 }
 
-function compare<T extends AllType, K extends AllType>(top: Pick<T, keyof AllType>, bottom: Pick<K, keyof AllType>): AllType {
-  return {
-    name: top.name,
+function compare<T extends Pick<AllType, "name" | "color">, U extends Pick<AllType, "position" | "weight">>(top: T, bottom: U): AllType { 
+  return {name: top.name,
     color: top.color,
     position: bottom.position,
-    weight: bottom.weight,
-  }
-}
+    weight: bottom.weight,}
+   }
+
